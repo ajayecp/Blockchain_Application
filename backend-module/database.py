ba613d462b35356a00ad852c360bd7d895f79cab
ajay-ecp <ajay9714@hotmail.com>
2026-05-22 15:47:46 -0400

    Feat: added db

--- backend-module/database.py
+++ backend-module/database.py
@@ -1,9 +1,12 @@
+import os
 from sqlalchemy import create_engine, Column, Integer, String, Float, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import json
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blockchain.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "blockchain.db")
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -11,14 +14,11 @@ Base = declarative_base()
 
 class BlockModel(Base):
     __tablename__ = "blocks"
-
     id = Column(String, primary_key=True, index=True)
     index = Column(Integer)
     timestamp = Column(Float)
     proof = Column(Integer)
     previous_hash = Column(String)
-    # Guardamos o dicionário 'dados'  texto JSON
     dados_json = Column(Text)
 
-# Cria a tabela se não existir
 Base.metadata.create_all(bind=engine)
